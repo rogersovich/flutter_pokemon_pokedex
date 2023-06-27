bf9515490a92599d4ff9b197154b225823d0de41
rogersovich <dimasroger89@gmail.com>
2023-06-27 11:00:53 +0700

    [UPDATE] Color & Text field widget

--- lib/utils/text_field_util.dart
+++ lib/utils/text_field_util.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_pokemon_pokedex/constants/color_constant.dart';
+import 'package:flutter_pokemon_pokedex/size_config.dart';
 import 'package:flutter_pokemon_pokedex/utils/text_util.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 
@@ -15,6 +17,12 @@ class CustomFormTextField extends StatelessWidget {
   final bool isReadonly;
   final bool isEnabled;
   final String align;
+  final double rounded;
+  final Color? borderColor;
+  final Color? borderColorActive;
+  final Color? borderColorError;
+  final double borderWidth;
+  final BorderStyle borderStyle;
   final List<FormFieldValidator>? validators;
 
   const CustomFormTextField({
@@ -30,11 +38,20 @@ class CustomFormTextField extends StatelessWidget {
     this.isReadonly = false,
     this.isEnabled = true,
     this.align = 'left',
+    this.rounded = 4,
+    this.borderColor,
+    this.borderColorActive,
+    this.borderColorError,
+    this.borderWidth = 0.8,
+    this.borderStyle = BorderStyle.solid,
     this.validators,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    BorderRadius borderRadius =
+        BorderRadius.circular(getProportionateScreenHeight(context, rounded));
+
     return FormBuilderTextField(
       key: key,
       name: name,
@@ -51,8 +68,27 @@ class CustomFormTextField extends StatelessWidget {
         fillColor: fillColor ?? Colors.grey[200],
         contentPadding:
             const EdgeInsets.symmetric(horizontal: 16.0, vertical: 0.0),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
+
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+              color: borderColor ?? kBorderColor,
+              width: borderWidth,
+              style: borderStyle),
+          borderRadius: borderRadius,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+              color: borderColorActive ?? kBorderColorActive,
+              width: borderWidth,
+              style: borderStyle),
+          borderRadius: borderRadius,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+              color: borderColorError ?? kBorderColorError,
+              width: borderWidth,
+              style: borderStyle),
+          borderRadius: borderRadius,
         ),
       ),
       validator: FormBuilderValidators.compose(validators ?? []),
